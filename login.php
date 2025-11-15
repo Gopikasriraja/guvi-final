73bf5e330a9d76bcd11a329fbf74bac8ad49c324
GOPIKA SRI D C <gopikasriraja1118@gmail.com>
2025-11-15 22:10:19 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,3 @@
 <?php
-$conn = pg_connect(getenv("DATABASE_URL"));
-
-$username = $_POST["username"];
-$password = $_POST["password"];
-
-$res = pg_query_params($conn, "SELECT password FROM users WHERE username=$1", [$username]);
-$row = pg_fetch_assoc($res);
-
-if ($row && password_verify($password, $row["password"])) {
-    echo "Login success";
-} else {
-    echo "Invalid credentials";
-}
+echo "Login working";
 ?>
